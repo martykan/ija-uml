2d87549cf698698e6a6cf2c72efca8021d06ccc8
Tomáš Martykán <t.martykan@gmail.com>
2022-03-15 08:19:51 +0100

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,8 @@
+# IJA UML app
+
+JavaFX application to create and edit UML class and sequence diagrams. 
+
+## Authors
+
+- Tomáš Martykán
+- Dominik Horký
---
+++ src/cz/vutfit/umlapp/MainClass.java
@@ -0,0 +1,7 @@
+package cz.vutfit.umlapp;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
