ba2c0e229838a8fda190b4955891d62787407b8d
Tomáš Martykán <t.martykan@gmail.com>
2022-05-06 22:23:37 +0200

    Remove relations when removing class

--- src/cz/vutfit/umlapp/model/commands/RemoveClassCommand.java
+++ src/cz/vutfit/umlapp/model/commands/RemoveClassCommand.java
@@ -6,14 +6,19 @@
 package cz.vutfit.umlapp.model.commands;
 
 import cz.vutfit.umlapp.model.uml.ClassDiagram;
+import cz.vutfit.umlapp.model.uml.Relationships;
 import cz.vutfit.umlapp.model.uml.UMLFileData;
 import cz.vutfit.umlapp.model.uml.exceptions.DuplicateClassNameException;
+import cz.vutfit.umlapp.model.uml.exceptions.DuplicateRelationshipException;
+
+import java.util.ArrayList;
 
 /**
  * Class for removing class command
  */
 public class RemoveClassCommand implements ICommand {
     private ClassDiagram removed;
+    private ArrayList<Relationships> removedRelationships;
     private final Integer classId;
 
     /**
@@ -35,6 +40,16 @@ public class RemoveClassCommand implements ICommand {
         this.removed = new ClassDiagram(this.classId);
         this.removed.setAll(name, x.getAttribs(), x.getMethods());
         file.removeClass(this.classId);
+
+        this.removedRelationships = new ArrayList<>();
+        for (Relationships relationship : file.getRelationships()) {
+            if (relationship.getFromClassID() == this.classId || relationship.getToClassID() == this.classId) {
+                removedRelationships.add(relationship);
+            }
+        }
+        for (Relationships relationship : this.removedRelationships) {
+            file.removeRelation(relationship.getID());
+        }
     }
 
     /**
@@ -45,11 +60,24 @@ public class RemoveClassCommand implements ICommand {
     public void undo(UMLFileData file) {
         if (removed != null) {
             try {
-                file.addClass(removed.getName());
+                int classId = file.addClass(removed.getName());
+                file.getClassByID(classId).setAll(removed.getName(), this.removed.getAttribs(), this.removed.getMethods());
+                for (Relationships relationship : removedRelationships) {
+                    int relationId;
+                    if (relationship.getFromClassID() == this.classId) {
+                        relationId = file.addRelation(classId, relationship.getToClassID(), relationship.getType());
+                    } else {
+                        relationId = file.addRelation(relationship.getFromClassID(), classId, relationship.getType());
+                    }
+                    file.getRelationByID(relationId).setName(relationship.name);
+                    file.getRelationByID(relationId).setFromDesc(relationship.fromDesc);
+                    file.getRelationByID(relationId).setToDesc(relationship.toDesc);
+                }
             } catch (DuplicateClassNameException e) {
                 System.out.println("DuplicateClassName EXCEPTION");
+            } catch (DuplicateRelationshipException e) {
+                System.out.println("DuplicateRelationshipException EXCEPTION");
             }
-            file.getClassByID(this.classId).setAll(removed.getName(), this.removed.getAttribs(), this.removed.getMethods());
         }
     }
 }
